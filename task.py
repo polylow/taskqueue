3c859cb2fa4193f91c279425e5b9180534195d6d
Ayush Shanker <ayushshanker@outlook.com>
2016-04-19 13:03:16 +0000

    added timers to Task class

--- task.py
+++ task.py
@@ -1,7 +1,10 @@
 import uuid
+import time
 
 class Task:
 	def __init__(self, data):
 		self.id = uuid.uuid4().hex
 		self.data = data
 		self.result = None
+		self.creation_time = time.time()
+		self.running_time = None
